39676808eed66af520fb3445e67d25d4d6b10bd3
W. Guilherme <wguilhermecriativo@gmail.com>
2022-03-31 11:37:33 -0300

    feature: add basic route to request data and mensure performance in milliseconds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import 'dotenv/config';
 import express from 'express';
+
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+
+
+app.get('/cat-facts', async (req, res) => {
+
+  // timer performance
+  const start = Date.now()
+  const { data: catFacts } = await axios.get('https://cat-fact.herokuapp.com/facts')
+  const end = Date.now()
+  const time = end - start
+  console.log(`Time taken to fetch cat facts: ${time}ms`)
+  res.status(200).json({ status: 'success', data: { facts: catFacts } })
+})
 
 app.get("/", (req, res) => {
   res.json({ status: 'sucess', message: 'API is running' })
